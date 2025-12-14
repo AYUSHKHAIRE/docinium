71f63ab99fe97c1ead366e7d72958d7d8fb553fd
Ayush Khaire <codeeayush@gmail.com>
2025-12-14 14:26:04 +0530

    websocket routing

---
+++ docinium/docinium_engine/connector/routing.py
@@ -0,0 +1,11 @@
+# chat/routing.py
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(
+        r"ws/docinium/(?P<user_id>[a-f0-9\-]+)/$", 
+        consumers.WebSocketConsumer.as_asgi()
+    ),
+]
