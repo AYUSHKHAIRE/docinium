c314daf2f3b4ca738e53c16ff65e1369cc5cbc23
Ayush Khaire <codeeayush@gmail.com>
2025-12-27 01:09:29 +0530

    test for container - working

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from docinium import DocShip
+from docinium import Container
 
-ship = DocShip(name="ship 1", port=23456)
+PORT = 23480
+
+ship = DocShip(name="ship 1", port=PORT)
 ship.dock()
+
+container = Container(
+    name = "container 1",
+    port_to_connect = PORT
+)
+
 ship.wait(500)
 
 ship.unDock()
