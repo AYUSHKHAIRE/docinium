e48d389dd01219c44156b960c268b3642912a307
Ayush Khaire <codeeayush@gmail.com>
2025-12-27 01:05:44 +0530

    attempt for utils

--- docinium/docinium_engine/connector/utils.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def get_local_ip():
-# Gets the primary network interface's IP address
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(("8.8.8.8", 80))  # Google's public DNS
-        local_ip = s.getsockname()[0]
-    return local_ip
