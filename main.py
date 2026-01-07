e98405d29fbf8fce461bcbac039e49f0b7d83584
Ayush Khaire <codeeayush@gmail.com>
2026-01-07 20:44:54 +0530

    guca test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from docinium import DocShip
 from docinium import Container
 
-PORT = 23476
+PORT = 23481
 
 ship = DocShip(name="ship 1", port=PORT)
 ship.dock()
@@ -11,7 +11,7 @@ container = Container(
     port_to_connect = PORT
 )
 
-
+ship.mount(container)
 
 ship.wait(500)
 
