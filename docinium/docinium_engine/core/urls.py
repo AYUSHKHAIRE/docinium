48d32902818c24877d738f84ce280c56a432c723
Ayush Khaire <codeeayush@gmail.com>
2026-04-15 22:59:01 +0530

    Add URL path for retrieving containers in the core application

--- docinium/docinium_engine/core/urls.py
+++ docinium/docinium_engine/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from core import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path("containers", views.containers, name="get_containers"),
     path('login', views.Login, name='login'),
     path('logout', views.Logout, name='logout'),
 ]
