4daa31308c0e95131df226c01a3f68df11fae44d
Ayush Khaire <codeeayush@gmail.com>
2026-04-15 22:58:38 +0530

    Add guacamole_client_token parameter to RDP connection registration URL

--- docinium/docinium_engine/connector/urls.py
+++ docinium/docinium_engine/connector/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import preregister_user_view, egister_rdp_connection_view
+from .views import preregister_user_view, egister_rdp_connection_view , display_all_rdp_connections_view
 
 urlpatterns = [
     path("api/preregister/", preregister_user_view, name="preregister_user"),
-    path("api/rdp-connection/<str:container_connected_name>/<int:identifier>/", egister_rdp_connection_view, name="register_rdp_connection"),
+    path("api/rdp-connection/<str:container_connected_name>/<int:identifier>/<str:guacamole_client_token>/", egister_rdp_connection_view, name="register_rdp_connection"),
+    path("api/containers/", display_all_rdp_connections_view, name="get_rdp_connections"),
 ]
