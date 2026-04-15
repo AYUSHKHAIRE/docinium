ce3e81250ae222ba826b2be49ed37a42e2ac09db
Ayush Khaire <codeeayush@gmail.com>
2026-04-15 21:22:15 +0530

    Add rdpConnection view to URL patterns for RDP connection registration

--- docinium/docinium_engine/connector/urls.py
+++ docinium/docinium_engine/connector/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import preregister_user_view
+from .views import preregister_user_view, egister_rdp_connection_view
 
 urlpatterns = [
     path("api/preregister/", preregister_user_view, name="preregister_user"),
+    path("api/rdp-connection/<str:container_connected_name>/<int:identifier>/", egister_rdp_connection_view, name="register_rdp_connection"),
 ]
