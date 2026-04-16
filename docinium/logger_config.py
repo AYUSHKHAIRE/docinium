8df31a1c91ca6d7c55d8ed7007aa2a615361a078
Ayush Khaire <codeeayush@gmail.com>
2026-04-17 00:31:43 +0530

    Refactor logger configuration to use loguru for improved logging management

--- docinium/logger_config.py
+++ docinium/logger_config.py
@@ -1,29 +1,9 @@
-import logging
-import colorlog
+from loguru import logger
 
-logger = colorlog.getLogger("docinium")
-logger.setLevel(logging.DEBUG)
-
-if not logger.handlers:
-    handler = colorlog.StreamHandler()
-
-    formatter = colorlog.ColoredFormatter(
-        "%(log_color)s%(asctime)s - %(levelname)s - %(message)s",
-        log_colors={
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "bold_red",
-        },
-    )
-
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-   
-
-# VERY IMPORTANT (prevents double logging via root logger)
-logger.propagate = False
-
-# Silence noisy libs
-# logging.getLogger("requests").setLevel(logging.WARNING)
+logger.add(
+    "docinium.log",
+    rotation="10 MB",
+    retention="7 days",
+    level="TRACE",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}"
+)
