5ea3bc3519cb9168c2ee3caf4eddf5b77fdb2db6
Ayush Khaire <codeeayush@gmail.com>
2026-04-20 21:30:32 +0530

    Add ScreenController class for mouse and keyboard control using PyAutoGUI

---
+++ docinium/container/utils/gui.py
@@ -0,0 +1,33 @@
+import time
+import loguru
+import pyautogui
+from utils.logger_config import logger
+
+class ScreenController:
+    def __init__(self):
+        self.screen_width, self.screen_height = pyautogui.size()
+
+    # Mouse control methods
+    def move_mouse(self, x, y):
+        pyautogui.moveTo(x, y)
+
+    def click_mouse(self, x, y):
+        pyautogui.click(x, y)
+
+    def click_based_on_image(self, image_base64):
+        location = pyautogui.locateOnScreen(image_base64, confidence=0.8)
+        if location:
+            pyautogui.click(location)
+        else:
+            logger.warning(f"Image '{image_base64}' not found on the screen.")
+
+    # Keyboard control methods
+    def type_text(self, text):
+        pyautogui.typewrite(text)
+
+    def press_key(self, key):
+        pyautogui.press(key)
+
+    # Screen information method
+    def get_screen_size(self):
+        return self.screen_width, self.screen_height
